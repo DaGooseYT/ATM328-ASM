6374a9dffc22659b89ef2cdf5bd10fe68b0ddc54
Goose <72713589+DaGooseYT@users.noreply.github.com>
2023-04-08 06:58:31 -0500

    Add files via upload

--- asm/demo.asm
+++ asm/demo.asm
@@ -1,13 +1,13 @@
-// demo file that lights LED at PINB0	
+; demo file that lights LED at PINB0	
 	
-	.device ATmega328P
+    .device ATmega328P
 
-	.cseg
-	.org 	0x0000
+    .cseg
+    .org 	0x0000
 
-	ldi		r16, (1 << PINB0)
+    ldi		r16, (1 << PINB0)
     out     DDRB, r16
     out     PORTB, r16
 
 Loop:
-	rjmp    Loop
+    rjmp    Loop
